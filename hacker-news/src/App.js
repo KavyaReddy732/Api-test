15b4e7e95c9c585d52622ffbb7f558c3049761d0
KavyaReddy732 <kavyareddy.732@gmail.com>
2021-09-27 22:12:26 +0200

    second commit

--- hacker-news/src/App.js
+++ hacker-news/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import FetchApi from './components/FetchApi';
-import User from './components/User';
 
 function App() {
   return (
     <div className="App">
       <h1>Hacker News</h1>
       <FetchApi></FetchApi>
-      <User />
     </div>
   );
 }
--- hacker-news/src/components/FetchApi.js
+++ hacker-news/src/components/FetchApi.js
@@ -1,33 +1,55 @@
 import { useState, useEffect } from 'react';
-import TopStories from './TopStories';
+import './stories.css'
 
 const FetchApi = () => {
-    const [state, setState] = useState([]);
-    console.log(state)
-
+    const [storyList, setStoryList] = useState([]);
 
     useEffect(() => {
         (async () => {
-            const result = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
-            const response = await result.json();
-            const shuffled = response.sort(() => 0.5 - Math.random());
-            const selected = shuffled.slice(0, 10);
-            setState(selected);
+            const result = await fetch(
+                "https://hacker-news.firebaseio.com/v0/topstories.json"
+            );
+            const userIdList = await result.json();
+            const shuffled = userIdList.sort(() => 0.5 - Math.random());
+            const slicedUserIdList = shuffled.splice(0, 10);
+
+            let stories = await Promise.all(
+                slicedUserIdList.map((id) => {
+                    return fetch(
+                        `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+                    ).then((res) => res.json());
+                })
+            );
+
+            stories = await Promise.all(
+                stories.map((story) => {
+                    return fetch(
+                        `https://hacker-news.firebaseio.com/v0/user/${story.by}.json`
+                    )
+                        .then((res) => res.json())
+                        .then((user) => {
+                            return { ...story, userId: user.id, userKarma: user.karma };
+                        });
+                })
+            );
+            setStoryList(stories);
         })();
     }, []);
-
     return (
-        <div>
-            {/* {state.map(element => <p>
-                {element}
-            </p>)} */}
-            {state.map((element) => (
-                <div>
-                    <TopStories state={element} />
-                </div>))}
+        <div className="list">
+            {storyList.sort((a, b) => a.score - b.score)
+                .map(({ id, by, title, time, userKarma, score, url }) => (
+                    <div key={id} className="grid-container">
+                        <p>Title : {title}</p>
+                        <p>Url : {url}</p>
+                        <p>score : {score}</p>
+                        <p>Time : {new Date(time).toLocaleTimeString("en-US")}</p>
+                        <p>Author : {by}</p>
+                        <p>Karma : {userKarma}</p>
+                    </div>
+                ))}
         </div>
-    )
+    );
 }
-
 
 export default FetchApi;
